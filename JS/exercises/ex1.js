e6cd8d092f315c1a3bec22154ca78c9faec18cc0
zahmor <zah.mor@gmail.com>
2021-01-24 16:08:13 +0200

    Apple

--- JS/exercises/ex1.js
+++
@@ -1,9 +0,0 @@
-//Tzach Mordechai
-//ex1
-//Input: user's name
-//Output: "Hello <UserName>!"
-
-
-let readlineSync = require('readline-sync');
-let userName = readlineSync.question('May I have your name? ');
-console.log('Hello ' + userName + '!');
--- JS/exercises/ex5.js
+++
@@ -1,8 +0,0 @@
-//Tzach Mordechai. 
-//ex5 
-//Input: no input.  
-//Output: all the odd numbers from 1 to 100.
-
-for (let i=0; i<100 ; i+=1){
-    (i%2===1)?console.log(i +', '):1;
-}
